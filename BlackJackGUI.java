17fc42b71db5573ce0d57c56c391bc6af7b9322a
Devin Monday <44091599+Mondayd@users.noreply.github.com>
2018-12-02 12:28:21 -0500

    A working Black Jack GUI.
    Still has some bugs in it, but it can run blackjack just fine.

--- BlackJackGUI.java
+++ BlackJackGUI.java
@@ -1,5 +1,3 @@
-package project;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,11 +11,17 @@ import java.io.*;
 
 public class BlackJackGUI extends JPanel implements ActionListener {
 
-    private JLabel title;
+    private boolean isTurn = false;
     BlackJack game;
     Player    user;
+    private JLabel title;
     private JLabel money;
     private JLabel bet;
+    private JLabel userAmt;
+    private JLabel dealerAmt;
+    
+    private int amt;
+    private final int LIMIT = 21;
     
     private JButton hitButton;
     private JButton stayButton;
@@ -26,7 +30,7 @@ public class BlackJackGUI extends JPanel implements ActionListener {
     
     private ArrayList<JLabel> playerHand = new ArrayList<JLabel>();
     private ArrayList<JLabel> dealerHand = new ArrayList<JLabel>();
-    private ArrayList<ImageIcon> visualDeck = new ArrayList<ImageIcon>();
+    private ArrayList<ImageIcon> visualDeck;
 
 
     public BlackJackGUI(Player player, ArrayList<ImageIcon> vDeck){
@@ -35,7 +39,7 @@ public class BlackJackGUI extends JPanel implements ActionListener {
         
         visualDeck = vDeck;
         
-        setPreferredSize(new Dimension(400,500));
+        setPreferredSize(new Dimension(800,600));
         
         setLayout(new GridBagLayout());
         GridBagConstraints loc = new GridBagConstraints();
@@ -44,6 +48,8 @@ public class BlackJackGUI extends JPanel implements ActionListener {
         title = new JLabel("BLACK JACK");
         money = new JLabel("Money:" + user.money);
         bet = new JLabel("Bet:");
+        userAmt = new JLabel("You: ");
+        dealerAmt = new JLabel ("Dealer: ");
         
         hitButton = new JButton("Hit");
         stayButton = new JButton("Stay");
@@ -83,36 +89,105 @@ public class BlackJackGUI extends JPanel implements ActionListener {
 
         add(playButton,loc);
         
-        for(int i = 0; i < 3; i++){
-            playerHand.add(new JLabel(visualDeck.get(52)));
+        loc.gridx = 0;
+        loc.gridy = 3;
+        
+        add(userAmt, loc);
+        
+        loc.gridy = 4;
+        add(dealerAmt, loc);
+        
+        for(int i = 1; i < 8; i++){
+            playerHand.add(new JLabel(visualDeck.get(53)));
+            dealerHand.add(new JLabel(visualDeck.get(53)));
             loc.gridx = i;
             loc.gridy = 3;
-            add(playerHand.get(i),loc);
+            add(playerHand.get(i-1),loc);
+            loc.gridy = 4;
+            add(dealerHand.get(i-1),loc);
         }
         
         hitButton.addActionListener(this);
         stayButton.addActionListener(this);
         playButton.addActionListener(this);
-        setBackground(new Color(0,99,0));
-
-
+        setBackground(new Color(0,99,0));  //Sets the background to a dark green color.
     }
-
+    
+    
+    //What I use to display the cards.
+    public void displayCards(){
+        int cardID;
+        int suitMultiplyer = 1;
+        
+        
+        int handSize = game.user.hand.size();
+        userAmt.setText("You: " + game.calcHand(game.user));
+        dealerAmt.setText("Dealer: " + game.calcHand(game.dealer));
+        for(int i = 0; i < handSize; i++){
+            switch(game.user.hand.get(i).suit){
+            case "Spades": suitMultiplyer = 0;
+            break;
+            case "Clubs": suitMultiplyer = 1;
+            break;
+            case "Hearts": suitMultiplyer = 2;
+            break;
+            case "Diamonds": suitMultiplyer = 3;
+            break;
+            } 
+            
+            cardID = game.user.hand.get(i).rank + suitMultiplyer * 13 - 2; //FORMULA TO GET THE RIGHT CARD ID.
+            playerHand.get(i).setIcon(visualDeck.get(cardID));
+        }
+        for(int j = handSize; j < 7; j++){
+            playerHand.get(j).setIcon(visualDeck.get(53));
+        }
+        handSize = game.dealer.hand.size();
+        
+        for(int i = 0; i < handSize; i++){
+            switch(game.dealer.hand.get(i).suit){
+            case "Spades": suitMultiplyer = 0;
+            break;
+            case "Clubs": suitMultiplyer = 1;
+            break;
+            case "Hearts": suitMultiplyer = 2;
+            break;
+            case "Diamonds": suitMultiplyer = 3;
+            break;
+            } 
+            
+            cardID = game.dealer.hand.get(i).rank + suitMultiplyer * 13 - 2; //FORMULA TO GET THE RIGHT CARD ID.
+            dealerHand.get(i).setIcon(visualDeck.get(cardID));
+        }
+        for(int j = handSize; j < 7; j++){
+            dealerHand.get(j).setIcon(visualDeck.get(53));
+        }
+    }
     public void actionPerformed(ActionEvent e){
         Object source = e.getSource();
         
-        if(source == playButton){
+        if(source == playButton && isTurn == false){
             Deck.getInstance();
             int amt = Integer.parseInt(wager.getText());
             game.playGameGUIUser(amt);
-            System.out.println("PLAY");
+            displayCards();
+            dealerHand.get(1).setIcon(visualDeck.get(52));
+            isTurn = true;
         }
         
-        else if(source == hitButton){
-            game.hit(user);
+        else if(source == hitButton && isTurn == true){
+            if(game.calcHand(game.user) <= LIMIT)
+                game.hit(user);
+            displayCards();
+            dealerHand.get(1).setIcon(visualDeck.get(52));
         }
-        else if(source == stayButton){
-            game.isStay = true;
+        else if((source == stayButton || game.calcHand(game.user) > LIMIT) && isTurn == true){
+            game.playGameGUIDealer(amt);
+            if(game.calcHand(game.user) < LIMIT)
+                displayCards();
+            
+            money.setText("Money:" + user.money);
+            game.reset();
+            isTurn = false;
         }
     }
 
